567f6e4a04ddc975fae632911d828395925d87ad
shehanp12 <shehanp068@gmail.com>
2020-08-10 22:46:08 +0530

    mysql function

---
+++ enterprise/src/enterpriseapp/mysql/auth/dao/EmployeeDao.java
@@ -0,0 +1,58 @@
+package enterpriseapp.mysql.auth.dao;
+
+import enterpriseapp.mysql.auth.model.User;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class EmployeeDao {
+
+    public int registerEmployee(User user) throws  ClassNotFoundException{
+
+        String INSERT_USERS_SQL="INSERT INTO user " +
+                "(name,email,pasword) VALUES (?,?,?)";
+
+        int result=0;
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        try (
+                Connection connection =
+                        DriverManager.getConnection("jdbc:mysql://localhost:3306/enterpriseapp","root","root");
+                PreparedStatement preparedStatement =connection.prepareStatement(INSERT_USERS_SQL)) {
+            //Step02:Create a statment using connection object
+//            preparedStatement.setInt(1,1);
+            preparedStatement.setString(1,user.getName());
+            preparedStatement.setString(2,user.getEmail());
+            preparedStatement.setString(3,user.getPassword());
+
+
+
+            System.out.println(preparedStatement);
+
+            //Step 03: Excute the query or update the query
+            result =preparedStatement.executeUpdate();
+
+
+        }
+
+
+
+
+
+
+
+
+        catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+
+        return  result;
+
+    }
+
+
+
+}
