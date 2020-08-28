a0b59e1815e3fb81cba368f9e9681c3be36a7ee5
Anjana-lakshan <55266848+Anjana-lakshan@users.noreply.github.com>
2020-08-28 23:54:02 +0530

    Create Post_ad.java

---
+++ enterprise/web/post add/Post_ad.java
@@ -0,0 +1,17 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Post_ad")
+public class Post_ad extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
